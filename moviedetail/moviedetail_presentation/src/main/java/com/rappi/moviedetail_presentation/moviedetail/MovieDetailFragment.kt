cf5949738e5fbd70e0f7bbe1045eb4647521b018
Kevin Perez <kevin.perez@tektonlabs.com>
2022-10-21 16:13:12 -0500

    adding parallax effect to movie detail page

--- moviedetail/moviedetail_presentation/src/main/java/com/rappi/moviedetail_presentation/moviedetail/MovieDetailFragment.kt
+++ moviedetail/moviedetail_presentation/src/main/java/com/rappi/moviedetail_presentation/moviedetail/MovieDetailFragment.kt
@@ -66,7 +66,7 @@ class MovieDetailFragment : DialogFragment() {
             )
         }
 
-        binding.backButton.setOnClickListener {
+        binding.appBar.setNavigationOnClickListener {
             dismiss()
         }
     }
